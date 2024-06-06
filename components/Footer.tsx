9581947d837ee0c58e66f618721a6f6d5c25132d
rcamarano <rogeriocamarano@gmail.com>
2024-06-06 11:40:54 -0300

    centering footer itens

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,16 +3,16 @@ export default function Footer({ footerRef }) {
     <footer ref={footerRef} className="center flex justify-center space-x-4 bg-[#cbcbcb] p-4 text-xs">
       <p>2023® - All rigths reserved</p>
       <span>|</span>
-      <a
+      <a href="window.open('https://www.sitelock.com/verify.php?site=camarano.net','SiteLock','width=600,height=600,left=160,top=170');" target="_blank" rel="noopener noreferrer"
+        className="font-medium text-orange-600" >
+        <img className="img-fluid" alt="SiteLock" title="SiteLock" src="https://shield.sitelock.com/shield/camarano.net" /></a>
+      <span>|</span>
+        <a
         href="https://www.camarano.net/wp" target="_blank" rel="noopener noreferrer"
         className="font-medium text-orange-600"
       >
         RC - Consultoria Digital
       </a>
-      <span>|</span>
-      <a href="window.open('https://www.sitelock.com/verify.php?site=camarano.net','SiteLock','width=600,height=600,left=160,top=170');" target="_blank" rel="noopener noreferrer"
-        className="font-medium text-orange-600" >
-        <img className="img-fluid" alt="SiteLock" title="SiteLock" src="https://shield.sitelock.com/shield/camarano.net" /></a>
     </footer>
   );
 }
